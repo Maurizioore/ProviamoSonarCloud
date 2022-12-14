344adea130d8edbe84fb0efef03b799c9b8b92aa
Maurizioore <mauriziorenzi68@gmail.com>
2022-12-14 11:14:15 +0100

    Calcolatrice che fa solo l'addizione di due numeri

---
+++ src/main/java/org/example/Calcolatore.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Calcolatore {
+    private String stringaInsignificante=" io sono una stringa che non serve a nulla, vediamo se sonar cloud mi toglie";
+    public void add(int a , int b){
+        System.out.println(a+b);
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calcolatore calcolatore=new Calcolatore();
+        calcolatore.add(3,4);
+    }
+}

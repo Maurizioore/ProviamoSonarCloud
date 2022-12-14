5d54468ecc4cdb891c3aeef619cd448ebcaa42b3
Maurizioore <mauriziorenzi68@gmail.com>
2022-12-14 11:19:51 +0100

    scriviamo fine

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Calcolatore calcolatore=new Calcolatore();
         calcolatore.add(3,4);
+        System.out.println("fine");
     }
 }

f921c3c7007d0736c0b1ab214d162cf3ccb88fdd
Maurizioore <mauriziorenzi68@gmail.com>
2022-12-14 11:45:31 +0100

    ho rimosso i println

--- src/main/java/org/example/Calcolatore.java
+++ src/main/java/org/example/Calcolatore.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Calcolatore {
     private String stringaInsignificante=" io sono una stringa che non serve a nulla, vediamo se sonar cloud mi toglie";
-    public void add(int a , int b){
-        System.out.println(a+b);
-        System.out.println("fine");
+    public int add(int a , int b){
+        return a+b;
+
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Calcolatore calcolatore=new Calcolatore();
         calcolatore.add(3,4);
-        System.out.println("fine");
     }
 }
